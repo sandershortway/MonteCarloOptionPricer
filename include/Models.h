343ac68a5aa5afa34f628525fd33baefb4ecbfb8
Korteweg, Sander <s.korteweg@zandersgroup.com>
2024-05-23 14:24:28 +0200

    added model framework, ready to be expanded

--- include/Models.h
+++ include/Models.h
@@ -1,30 +1,58 @@
 #pragma once
 
-class Model
+class VolatilityModel
 {
+private:
 	double Volatility;
 
 public:
+	VolatilityModel(double InitialVol);
+	double GetCurrentVolatility();
+};
+
+class DeterministicModel : public VolatilityModel
+{
+public:
+	DeterministicModel(double InitialVol);
 	virtual double ComputeNextVolatility() = 0;
 };
 
-class BlackScholes : public Model
+class StochVolModel : public VolatilityModel
+{
+public:
+	StochVolModel(double InitialVol);
+	virtual double ComputeNextVolatility(double VolNormal) = 0;
+};
+
+class BlackScholes : public DeterministicModel
 {
 	double Sigma;
 
 public:
-	BlackScholes(double Sigma);
+	BlackScholes(double InitialVol, double Sigma);
 	double ComputeNextVolatility() override;
 };
 
-class SABR : public Model
+class Heston : public StochVolModel
 {
-	double Alpha;
-	double Beta;
-	double Rho;
-	double Nu;
+	double MeanReversionSpeed;
+	double LongTermVariance;
+	double VolVol;
+	double Correlation;
 
 public:
-	SABR(double Alpha, double Beta, double Rho, double Nu);
-	double ComputeNextVolatility(double VolNormal);
+	Heston(double InitialVol, double MeanReversionSpeed, double LongTermVariance, double VolVol, double Correlation);
+	double ComputeNextVolatility(double VolNormal) override;
 };
+
+//class SABR : public StochVolModel
+//{
+//	double Alpha;
+//	double Beta;
+//	double Rho;
+//	double Nu;
+//
+//public:
+//	SABR(double Alpha, double Beta, double Rho, double Nu);
+//	double ComputeNextVolatility(double VolNormal) override;
+//};
--- src/Models.cpp
+++ src/Models.cpp
@@ -1,10 +1,36 @@
 #include "Models.h"
 
-BlackScholes::BlackScholes(double Sigma) : Sigma(Sigma) {};
+double VolatilityModel::GetCurrentVolatility() { return Volatility; }
+
+VolatilityModel::VolatilityModel(double InitialVol) { Volatility = InitialVol; }
+
+DeterministicModel::DeterministicModel(double InitialVol) : VolatilityModel(InitialVol) {};
+
+StochVolModel::StochVolModel(double InitialVol) : VolatilityModel(InitialVol) {};
+
+Heston::Heston(double InitialVol, double MeanReversionSpeed, double LongTermVariance, double VolVol, double Correlation)
+	:
+	StochVolModel(InitialVol),
+	MeanReversionSpeed(MeanReversionSpeed),
+	LongTermVariance(LongTermVariance),
+	VolVol(VolVol),
+	Correlation(Correlation) {};
+
+double Heston::ComputeNextVolatility(double VolNormal)
+{
+	return VolNormal;
+}
+
+BlackScholes::BlackScholes(double InitialVol, double Sigma) : Sigma(Sigma), DeterministicModel(InitialVol) {};
 
 double BlackScholes::ComputeNextVolatility()
 {
 	return Sigma;
 }
 
-SABR::SABR(double Alpha, double Beta, double Rho, double Nu) : Alpha(Alpha), Beta(Beta), Rho(Rho), Nu(Nu) {};
+//SABR::SABR(double Alpha, double Beta, double Rho, double Nu) : Alpha(Alpha), Beta(Beta), Rho(Rho), Nu(Nu) {};
+//
+//double SABR::ComputeNextVolatility(double VolNormal)
+//{
+//	return VolNormal;
+//}
